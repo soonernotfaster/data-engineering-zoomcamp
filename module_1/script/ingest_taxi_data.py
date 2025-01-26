6415e67b45810927c8507ed78f67dd856464a6e2
Steven Solomon <solomon.steven.m@gmail.com>
2025-01-26 17:51:01 -0500

    Ingest zone data

--- module_1/script/ingest_taxi_data.py
+++ module_1/script/ingest_taxi_data.py
@@ -6,23 +6,24 @@ from sqlalchemy import create_engine, Engine, Connection
 from os import PathLike
 
 DATE_COLUMNS = ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
-INPUT_PATH = "module_1/docker_intro/data/yellow_tripdata_2021-01.csv"
+YELLOW_CAB_CSV = "module_1/docker_intro/data/yellow_tripdata_2021-01.csv"
+ZONE_CSV = "module_1/docker_intro/data/taxi_zone_lookup.csv"
 CONNECTION_STRING = "postgresql://root:root@localhost:5432/ny_taxi"
-TABLE_NAME = "yellow_taxi_data"
 
-def insert_data(connection: Connection, path: PathLike[str]) -> None:
+def insert_data(connection: Connection, path: PathLike[str], table_name: str, date_columns: list[str] = None) -> None:
     """
     Inserts data in chunks from the provided path.g
     """
-    data_iters = pd.read_csv(path, parse_dates=DATE_COLUMNS, chunksize=100_000, iterator=True)
+    data_iters = pd.read_csv(path, parse_dates=date_columns, chunksize=100_000, iterator=True)
     
     for df in tqdm(data_iters):
-        df.to_sql(name=TABLE_NAME, con=connection, if_exists="append")
+        df.to_sql(name=table_name, con=connection, if_exists="append")
 
 def ingest() -> None:
     engine = create_engine(CONNECTION_STRING)
     with engine.begin() as connection:
-        insert_data(connection, INPUT_PATH)
+        insert_data(connection, YELLOW_CAB_CSV, table_name="yellow_taxi_data", date_columns = DATE_COLUMNS)
+        insert_data(connection, ZONE_CSV, table_name="zone")
 
 if __name__ == "__main__":
     ingest()
