5b145828f8c6e0ccc4ce8a698aae8513e4ef414f
Steven Solomon <solomon.steven.m@gmail.com>
2025-01-26 17:30:48 -0500

    Remove scripts replaced by compose

--- module_1/script/pg_admin.sh
+++
@@ -1,9 +0,0 @@
- #!/usr/bin/env bash
-
- docker run -it \
- -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
- -e PGADMIN_DEFAULT_PASSWORD="root" \
- -p 8080:80 \
- --network pg-network \
- --name pg-admin \
- dpage/pgadmin4
