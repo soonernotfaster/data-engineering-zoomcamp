003583de03f467c17680a022713c268ab6243c08
Steven Solomon <solomon.steven.m@gmail.com>
2025-02-10 11:03:57 -0500

    set output dir for state

--- script/tf_apply.sh
+++ script/tf_apply.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-WORK_DIR="../module_1/terra_demo"
+WORK_DIR="module_1/terra_demo"
 
 pushd $WORK_DIR
-terraform apply --var-file=./deploy.tfvars
+terraform apply --var-file=./deploy.tfvars --state=tf_state/terraform.tf_state
 popd
