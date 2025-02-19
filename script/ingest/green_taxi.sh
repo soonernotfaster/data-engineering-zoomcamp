6f4770f5c05463fb0e10e4a4520d1dae8b00f814
Steven Solomon <solomon.steven.m@gmail.com>
2025-02-19 12:26:45 -0500

    Adding script to download green taxi data

--- script/ingest/green_taxi.sh
+++ script/ingest/green_taxi.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
 
+DEST=`mktemp`
+
+GREEN_TRIPS="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2019-10.csv.gz"
+ZONES="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv"
+
+wget $GREEN_TRIPS --directory-prefix=$DEST
+wget $ZONES --directory-prefix=$DEST
+
+echo "Data at: $DEST"
+
+gunzip "$DEST/$(basename $GREEN_TRIPS)"
+
