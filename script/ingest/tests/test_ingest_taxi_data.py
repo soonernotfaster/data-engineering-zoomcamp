ba5e84e6663806f2e01694d6809700b3c77368ce
Steven Solomon <solomon.steven.m@gmail.com>
2025-02-19 15:28:02 -0500

    Extract arg variables

--- script/ingest/tests/test_ingest_taxi_data.py
+++ script/ingest/tests/test_ingest_taxi_data.py
@@ -2,6 +2,9 @@ import pytest
 import re
 from script.ingest.ingest_taxi_data import args
 
+TABLE_ARGS = ["-t", "table_name"]
+FILENAME_ARGS = ["-f", "in.csv"]
+
 def test_all_args_missing(capsys):
     with pytest.raises(SystemExit):
         args([])
@@ -12,20 +15,20 @@ def test_all_args_missing(capsys):
 
 def test_table_name_missing(capsys):
     with pytest.raises(SystemExit):
-        args(["-f", "in.csv"])
+        args(FILENAME_ARGS)
     
     _out, err = capsys.readouterr()
     assert re.match("usage:.*", err)
 
 def test_filename_missing(capsys):
     with pytest.raises(SystemExit):
-        args(["-t", "table_name"])
+        args(TABLE_ARGS)
     
     _out, err = capsys.readouterr()
     assert re.match("usage:.*", err)
 
 def test_all_args_provided():
-    result = args(["-f", "in.csv", "-t", "table_name"])
+    result = args(TABLE_ARGS + FILENAME_ARGS)
 
-    assert result.filename == "in.csv"
-    assert result.table == "table_name"
+    assert result.filename == FILENAME_ARGS[-1]
+    assert result.table == TABLE_ARGS[-1]
