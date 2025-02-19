319e6fa9b7b1ea1df0f0f4bb1896ff438b35a105
Steven Solomon <solomon.steven.m@gmail.com>
2025-02-19 13:19:43 -0500

    Move python script up and add test setup

---
+++ script/ingest/tests/test_ingest_taxi_data.py
@@ -0,0 +1,9 @@
+import unittest
+from script.ingest.ingest_taxi_data import args
+
+class TestIngestTaxiData(unittest.TestCase):
+    def test_foo(self):
+        self.assertFalse(True)
+
+if __name__ == "__main__":
+    unittest.main()
