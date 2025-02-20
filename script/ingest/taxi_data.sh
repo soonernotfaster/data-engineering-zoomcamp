92a79a3d0682a3a8a5754b29f7cd07759461f0fd
Steven Solomon <solomon.steven.m@gmail.com>
2025-02-20 10:16:50 -0500

    Extract function for fetching and uncompressing data

--- script/ingest/taxi_data.sh
+++ script/ingest/taxi_data.sh
@@ -2,20 +2,40 @@
 
 DEST=`mktemp`
 
-GREEN_TRIPS_GZ="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2019-10.csv.gz"
-ZONES_CSV="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv"
+GREEN_TRIPS_SOURCE="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2019-10.csv.gz"
+ZONES_SOURCE="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv"
+YELLOW_TRIPS_SOURCE="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
 
-GREEN_TRIPS_CSV=`basename -- "$GREEN_TRIPS_GZ" .gz`
-echo $GREEN_TRIPS_CSV
+function csv_for_trip() {
+    local DEST=$1
+    local TRIP_SOURCE=$2
+    local FILE_OUTPUT=`basename -- "$TRIP_SOURCE" .gz`
 
-wget $GREEN_TRIPS_GZ --directory-prefix=$DEST
-wget $ZONES_CSV --directory-prefix=$DEST
+    wget $TRIP_SOURCE --directory-prefix=$DEST
+
+    gunzip "$DEST/$(basename $TRIP_SOURCE)"
+    
+    echo $FILE_OUTPUT
+}
+
+GREEN_TRIPS_CSV=$(csv_for_trip $DEST $GREEN_TRIPS_SOURCE)
+YELLOW_TRIPS_CSV=$(csv_for_trip $DEST $YELLOW_TRIPS_SOURCE)
+
+wget $ZONES_SOURCE --directory-prefix=$DEST
+ZONES_CSV=`basename $ZONES_SOURCE`
 
 echo "Data at: $DEST"
-
-gunzip "$DEST/$(basename $GREEN_TRIPS_GZ)"
 
 ./script/ingest/ingest_taxi_data.py \
     -f "$DEST/$GREEN_TRIPS_CSV" \
     -t green_taxi_trips \
     --date-cols lpep_dropoff_datetime lpep_pickup_datetime
+
+./script/ingest/ingest_taxi_data.py \
+    -f "$DEST/$ZONES_CSV" \
+    -t taxi_zones
+
+./script/ingest/ingest_taxi_data.py \
+    -f "$DEST/$YELLOW_TRIPS_CSV" \
+    -t yellow_taxi_trips \
+    --date-cols tpep_pickup_datetime tpep_dropoff_datetime
