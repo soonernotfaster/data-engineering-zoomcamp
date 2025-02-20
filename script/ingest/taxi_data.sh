c8e6f838f837d66963e7e5a000e50982ac0dd8ea
Steven Solomon <solomon.steven.m@gmail.com>
2025-02-20 09:03:27 -0500

    Use generalized upload script

--- script/ingest/taxi_data.sh
+++ script/ingest/taxi_data.sh
@@ -2,14 +2,20 @@
 
 DEST=`mktemp`
 
-GREEN_TRIPS="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2019-10.csv.gz"
-ZONES="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv"
+GREEN_TRIPS_GZ="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2019-10.csv.gz"
+ZONES_CSV="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv"
 
-wget $GREEN_TRIPS --directory-prefix=$DEST
-wget $ZONES --directory-prefix=$DEST
+GREEN_TRIPS_CSV=`basename -- "$GREEN_TRIPS_GZ" .gz`
+echo $GREEN_TRIPS_CSV
+
+wget $GREEN_TRIPS_GZ --directory-prefix=$DEST
+wget $ZONES_CSV --directory-prefix=$DEST
 
 echo "Data at: $DEST"
 
-gunzip "$DEST/$(basename $GREEN_TRIPS)"
+gunzip "$DEST/$(basename $GREEN_TRIPS_GZ)"
 
-./01-docker-terraform/script/ingest_taxi_data.py -f 01-docker-terraform/docker_intro/data/taxi_zone_lookup.csv  -t wobb
+./script/ingest/ingest_taxi_data.py \
+    -f "$DEST/$GREEN_TRIPS_CSV" \
+    -t green_taxi_trips \
+    --date-cols lpep_dropoff_datetime lpep_pickup_datetime
